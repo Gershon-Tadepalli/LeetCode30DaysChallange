447baec2fa6c32b83562a05f22fd3a9e974e2f06
Gershon Tadepalli <gershon.tadepalli@toyota.com>
2020-05-02 18:17:28 +0530

    Day-1 Challange Completed

---
+++ Problems/Problems/Day-1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Problems
+{
+    class Day_1_FirstBadVersion
+    {
+        public int firstBadVersion { get; set; }
+        //There are N versions available need to find out which is the first bad version
+        //Ex : n = 5 and version 4 is the first bad version
+        // You should minimize the number of calls to the IsBadVersion API.
+        public int FirstBadVersion(int n)
+        {
+            int left=1, right=n, mid;
+            while (left < right)
+            {
+                //Handling overflow condition for bigger numbers which crossed max of int
+                mid = left + (right-left) / 2;
+                if(IsBadVersion(mid))
+                {
+                    //move left
+                    right = mid;
+                }
+                else
+                {
+                    //move right
+                    left = mid + 1;
+                }
+            }
+            return left;
+        }
+
+        private bool IsBadVersion(int version)
+        {
+            if(version >= firstBadVersion)
+              return true;
+            return false;
+        }
+    }
+}
---
+++ Problems/Problems/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Problems
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            //2126753390
+            //1702766719
+            //Given n = 5, and version = 4 is the first bad version.
+            Stopwatch watch = new Stopwatch();
+            Day_1_FirstBadVersion day_1 = new Day_1_FirstBadVersion();
+            day_1.firstBadVersion = 1702766719;
+            watch.Start();
+            int firstBadVersion =  day_1.FirstBadVersion(2126753390);
+            watch.Stop();
+            Console.WriteLine($"First Bad Version : {firstBadVersion} and time taken : {watch.ElapsedMilliseconds}");
+        }
+    }
+}
