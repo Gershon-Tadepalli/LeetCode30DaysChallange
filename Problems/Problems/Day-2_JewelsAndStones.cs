94754169806eeb130f61f690a01e5693163df867
Gershon Tadepalli <gershon.tadepalli@toyota.com>
2020-05-02 18:41:47 +0530

    Day-2 Challange Completed

---
+++ Problems/Problems/Day-2_JewelsAndStones.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problems
+{
+    class Day_2_JewelsAndStones
+    {
+        //J - Jewels, S - stones
+        //In S,You want to know how many of the stones you have are also jewels?
+        //Input: J = "aA", S = "aAAbbbb"
+        //Output: 3
+        public int NumJewelsInStones(string J, string S)
+        {
+            //Find each stone in S if it is Jewel or not by searching in J
+            int count = 0;
+            foreach (char c in S)
+            {
+                if (J.IndexOf(c) != -1)
+                    count++;
+            }
+            return count;
+        }
+    }
+}
--- Problems/Problems/Startup.cs
+++ Problems/Problems/Startup.cs
@@ -7,6 +7,7 @@ namespace Problems
     {
         static void Main(string[] args)
         {
+            #region Day-1
             //2126753390
             //1702766719
             //Given n = 5, and version = 4 is the first bad version.
@@ -17,6 +18,17 @@ namespace Problems
             int firstBadVersion =  day_1.FirstBadVersion(2126753390);
             watch.Stop();
             Console.WriteLine($"First Bad Version : {firstBadVersion} and time taken : {watch.ElapsedMilliseconds}");
+            #endregion
+
+            #region Day-2
+            Day_2_JewelsAndStones day_2 = new Day_2_JewelsAndStones();
+            string J = "z";
+            string S = "ZZ";
+            watch.Start();
+            int numJewelsInStones = day_2.NumJewelsInStones(J, S);
+            watch.Stop();
+            Console.WriteLine($"Number of Jewels in Stones : {numJewelsInStones} in time : {watch.ElapsedMilliseconds}");
+            #endregion
         }
     }
 }
